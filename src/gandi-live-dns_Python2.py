05527d6a8573a79d2560e2588dfdec28b87389d3
root <contact@tomadelie.fr>
2020-11-20 15:38:05 -0800

    Add version for Python 3

--- src/gandi-live-dns_Python2.py
+++ src/gandi-live-dns_Python2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # encoding: utf-8
 '''
 Gandi v5 LiveDNS - DynDNS Update via REST API and CURL/requests
@@ -15,7 +15,6 @@ http://doc.livedns.gandi.net/#api-endpoint -> https://dns.gandi.net/api/v5/
 import requests, json
 import config
 import argparse
-import os
 
 
 def get_dynip(ifconfig_provider):
@@ -23,8 +22,8 @@ def get_dynip(ifconfig_provider):
     similar to curl ifconfig.me/ip, see example.config.py for details to ifconfig providers 
     ''' 
     r = requests.get(ifconfig_provider)
-    print ('Checking dynamic IP: ' , r._content.decode('utf8').strip('\n'))
-    return r.content.decode('utf8').strip('\n')
+    print 'Checking dynamic IP: ' , r._content.strip('\n')
+    return r.content.strip('\n')
 
 def get_uuid():
     ''' 
@@ -39,11 +38,11 @@ def get_uuid():
     if u.status_code == 200:
         return json_object['zone_uuid']
     else:
-        print ('Error: HTTP Status Code ', u.status_code, 'when trying to get Zone UUID')
-        print  (json_object['message'])
+        print 'Error: HTTP Status Code ', u.status_code, 'when trying to get Zone UUID'
+        print  json_object['message']
         exit()
 
-def get_dnsip(uuid, subdomain):
+def get_dnsip(uuid):
     ''' find out IP from first Subdomain DNS-Record
     List all records with name "NAME" and type "TYPE" in the zone UUID
     GET /zones/<UUID>/records/<NAME>/<TYPE>:
@@ -52,16 +51,16 @@ def get_dnsip(uuid, subdomain):
     the actual DNS Record IP
     '''
 
-    url = config.api_endpoint+ '/zones/' + uuid + '/records/' + subdomain + '/A'
+    url = config.api_endpoint+ '/zones/' + uuid + '/records/' + config.subdomains[0] + '/A'
     headers = {"X-Api-Key":config.api_secret}
     u = requests.get(url, headers=headers)
     if u.status_code == 200:
         json_object = json.loads(u._content)
-        print ('Checking IP from DNS Record' , subdomain, ':', json_object['rrset_values'][0].encode('ascii','ignore').decode('utf8').strip('\n'))
-        return json_object['rrset_values'][0].encode('ascii','ignore').decode('utf8').strip('\n')
+        print 'Checking IP from DNS Record' , config.subdomains[0], ':', json_object['rrset_values'][0].encode('ascii','ignore').strip('\n')
+        return json_object['rrset_values'][0].encode('ascii','ignore').strip('\n')
     else:
-        print ('Error: HTTP Status Code ', u.status_code, 'when trying to get IP from subdomain', subdomain)
-        print (json_object['message'])
+        print 'Error: HTTP Status Code ', u.status_code, 'when trying to get IP from subdomain', config.subdomains[0]   
+        print  json_object['message']
         exit()
 
 def update_records(uuid, dynIP, subdomain):
@@ -81,37 +80,42 @@ def update_records(uuid, dynIP, subdomain):
     json_object = json.loads(u._content)
 
     if u.status_code == 201:
-        print ('Status Code:', u.status_code, ',', json_object['message'], ', IP updated for', subdomain)
+        print 'Status Code:', u.status_code, ',', json_object['message'], ', IP updated for', subdomain
         return True
     else:
-        print ('Error: HTTP Status Code ', u.status_code, 'when trying to update IP from subdomain', subdomain)
-        print (json_object['message'])
+        print 'Error: HTTP Status Code ', u.status_code, 'when trying to update IP from subdomain', subdomain   
+        print  json_object['message']
         exit()
 
 
 
-def main(force_update, verbosity):
+def main(force_update, verbosity, manual_ip):
 
     if verbosity:
-       print ("verbosity turned on - not implemented by now")
+        print "verbosity turned on - not implemented by now"
 
         
     #get zone ID from Account
     uuid = get_uuid()
 
-    dynIP = get_dynip(config.ifconfig) # Get Dynamic IP from lookup service
+    #compare dynIP and DNS IP
+    if manual_ip is None:
+        dynIP = get_dynip(config.ifconfig) # Get Dynamic IP from lookup service
+    else:
+        dynIP = manual_ip # Use provided IP address
+
+    dnsIP = get_dnsip(uuid)
     
     if force_update:
-        print ("Going to update/create the DNS Records for the subdomains")
+        print "Going to update/create the DNS Records for the subdomains"
         for sub in config.subdomains:
             update_records(uuid, dynIP, sub)
     else:
-        for sub in config.subdomains:
-            dnsIP = get_dnsip(uuid, sub)
-            if dynIP == dnsIP:
-                print ("IP Address Match for subdomain " + sub + " - no further action")
-            else:
-                print ("IP Address Mismatch for subdomain " + sub + " - going to update the DNS Records for the subdomains with new IP " + dynIP)
+        if dynIP == dnsIP:
+            print "IP Address Match - no further action"
+        else:
+            print "IP Address Mismatch - going to update the DNS Records for the subdomains with new IP", dynIP
+            for sub in config.subdomains:
                 update_records(uuid, dynIP, sub)
 
 if __name__ == "__main__":
@@ -120,20 +124,5 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--force', help="force an update/create", action="store_true")
     parser.add_argument('-i', '--ip', help="manually set the IP address, skip external lookup service")
     args = parser.parse_args()
-    main(args.force, args.verbose)
 
-'''
-    api_key = os.environ.get('GANDI_API_KEY')
-    domain = os.environ.get('GANDI_DOMAIN')
-    subdomains = os.environ.get('GANDI_SUBDOMAINS')
-
-    if((api_key is None) or (domain is None) or (subdomains is None)):
-        print ("Please set the following environement variables to configure the program : ")
-        print ("   - GANDI_API_KEY : The API key provided by gandi")
-        print ("   - GANDI_DOMAIN : The domain name that you would like to update. Ex : google.com")
-        print ("   - GANDI_SUBDOMAINS : The list of subdomains that you would like to update. Ex : www www2 home")
-    else:
-        config.api_secret = api_key
-        config.domain = domain
-        config.subdomains = subdomains.split(" ")
-'''
+    main(args.force, args.verbose, args.ip)
